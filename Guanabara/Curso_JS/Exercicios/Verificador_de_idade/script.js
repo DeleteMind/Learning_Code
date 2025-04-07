a2a448ed7f7631c4c7090744a365c6827e0586e9
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-04-07 15:43:35 +0100

    change_verificador_de_idade

--- Guanabara/Curso_JS/Exercicios/Verificador_de_idade/script.js
+++ Guanabara/Curso_JS/Exercicios/Verificador_de_idade/script.js
@@ -16,22 +16,23 @@ function checkIdade() {
     return
   }
 
+  let checkSexo = masc.checked ? "masculino" :  femi.checked ? 'Feminino' : 'de género não especificado';
 
 
   if (idade <= 2) {
     //Bebé
-    res.innerHTML = `Detectamos um bebé com ${idade}`;
+    res.innerHTML = `Detectamos um bebé ${checkSexo} com ${idade}`;
   } else if (idade <= 12) {
     //criança
-    res.innerHTML = `Detectamos uma criança com ${idade}`;
+    res.innerHTML = `Detectamos uma criança ${checkSexo} com ${idade}`;
   } else if (idade <= 17) {
     //adolescente
-    res.innerHTML = `Detectamos um adolescente com ${idade}`;
+    res.innerHTML = `Detectamos um adolescente ${checkSexo} com ${idade}`;
   } else if (idade < 60) {
     //adulto
-    res.innerHTML = `Detectamos um adulto com ${idade}`;
+    res.innerHTML = `Detectamos um adulto ${checkSexo} com ${idade}`;
   } else {
-    res.innerHTML = `Detectamos um idoso com ${idade}`;
+    res.innerHTML = `Detectamos um idoso ${checkSexo} com ${idade}`;
     //idoso
   }
 }
