c90cd8e839cc54e3ff14da35cb7f7bbb90107e50
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-04-10 14:21:24 +0100

    change_age_guessing

--- Guanabara/Curso_JS/Exercicios/Verificador_de_idade/script.js
+++ Guanabara/Curso_JS/Exercicios/Verificador_de_idade/script.js
@@ -3,6 +3,7 @@ let masc = document.getElementById("masc");
 let femi = document.getElementById("femi");
 let sub = document.getElementById("submit");
 let res = document.getElementById("resultado");
+let img = document.getElementById("imagem");
 
 sub.addEventListener("click", checkIdade);
 
@@ -11,28 +12,46 @@ function checkIdade() {
   let anoNascimento = Number(ano.value);
   let idade = anoActual - anoNascimento;
 
-  if (!anoNascimento || anoNascimento > anoActual ) {
-    res.innerHTML = 'Por favor, digite um ano válido';
-    return
+  if (!anoNascimento || anoNascimento > anoActual) {
+    res.innerHTML = "Por favor, digite um ano válido";
+    return;
   }
 
-  let checkSexo = masc.checked ? "masculino" :  femi.checked ? 'Feminino' : 'de género não especificado';
+  // verifica se o input está selecionado ou não
+  let checkSexo = masc.checked
+    ? "masculino"
+    : "feminino";
 
 
   if (idade <= 2) {
     //Bebé
     res.innerHTML = `Detectamos um bebé ${checkSexo} com ${idade}`;
+    checkSexo === "masculino"
+      ? (img.src = "Fotos/bebeM.jpg")
+      : (img.src = "Fotos/bebeF.jpg");
   } else if (idade <= 12) {
     //criança
     res.innerHTML = `Detectamos uma criança ${checkSexo} com ${idade}`;
+    checkSexo === "masculino"
+      ? (img.src = "Fotos/criançaM.jpg")
+      : (img.src = "Fotos/criançaF.jpg");
   } else if (idade <= 17) {
     //adolescente
     res.innerHTML = `Detectamos um adolescente ${checkSexo} com ${idade}`;
+    checkSexo === "masculino"
+      ? (img.src = "Fotos/jovemM.jpg")
+      : (img.src = "Fotos/jovemF.jpg");
   } else if (idade < 60) {
     //adulto
     res.innerHTML = `Detectamos um adulto ${checkSexo} com ${idade}`;
+    checkSexo === "masculino"
+      ? (img.src = "Fotos/adultoM.jpg")
+      : (img.src = "Fotos/adultoF.jpg");
   } else {
-    res.innerHTML = `Detectamos um idoso ${checkSexo} com ${idade}`;
     //idoso
+    res.innerHTML = `Detectamos um idoso ${checkSexo} com ${idade}`;
+    checkSexo === "masculino"
+      ? (img.src = "Fotos/idosoM.jpg")
+      : (img.src = "Fotos/idosaF.jpg");
   }
 }
