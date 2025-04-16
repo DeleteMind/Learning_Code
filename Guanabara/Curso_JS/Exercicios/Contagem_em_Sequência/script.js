a56662caf8ddd82af91835f6f8a3aab2d183ad48
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-04-16 15:03:32 +0100

    change_contagem_em_sequencia

--- Guanabara/Curso_JS/Exercicios/Contagem_em_Sequência/script.js
+++ Guanabara/Curso_JS/Exercicios/Contagem_em_Sequência/script.js
@@ -1,17 +1,20 @@
-let inicio = Number(document.getElementById('inicio').value);
-let fim = Number(document.getElementById('fim').value);
-let passo = Number(document.getElementById('passo').value);
-let contar = document.getElementById('contar');
- let output = document.getElementById('output');
+let contar = document.getElementById("contar");
+let output = document.getElementById("output");
 
-contar.addEventListener('click', começar);
-
-
+contar.addEventListener("click", começar);
 
 function começar() {
+  let inicio = Number(document.getElementById("inicio").value);
+  let fim = Number(document.getElementById("fim").value);
+  let passo = Number(document.getElementById("passo").value);
 
-   for (let i = inicio; i <= fim; i += passo) {
-        output.innerHTML = `${i}`;
+  output.innerHTML = "";
+
+  if (!inicio || !fim || !passo) {
+    output.innerHTML = "Por favor, preencha todos os passos";
+  } else {
+    for (let i = inicio; i <= fim; i += passo) {
+      output.innerHTML += `${i} `;
     }
-
+  }
 }
