b0ad7b0558b530f69767e3e3ab29c839d133d3bd
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-04-22 14:21:12 +0100

    add_tabuada

---
+++ Guanabara/Curso_JS/Exercicios/Tabuada/script.js
@@ -0,0 +1,22 @@
+let button = document.getElementById("gerarTab");
+let tabuada = document.getElementById("seltab");
+button.addEventListener("click", gerar);
+
+function gerar() {
+  let num = document.getElementById("numeroSelecionado");
+
+  tabuada.innerHTML = "";
+
+  if (num.value.length == 0) {
+    window.alert("Digite um número");
+  } else {
+    let n = Number(num.value);
+
+    for (let c = 1; c <= 10; c++) {
+      let item = document.createElement("option");
+      item.text = `${n} x ${c} = ${c * n}`
+      item.value = tab`${c}` // mais tarde para linguagem commo php poderá ser necessário para o programa recohecer o que foi selecionado
+      tabuada.appendChild(item);
+    }
+  }
+}
