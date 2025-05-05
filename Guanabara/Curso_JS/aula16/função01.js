740e064f2e5ad1a5d3fa6099089e9cb4276281ae
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-05-05 13:04:52 +0100

    add_aula016

---
+++ Guanabara/Curso_JS/aula16/função01.js
@@ -0,0 +1,13 @@
+
+
+
+function parim(num) {
+    let res = '';
+    if ( num % 2 === 0) {
+        res = 'Par' 
+    } else {
+        res = 'Impar';
+    }
+    return res;
+}
+console.log(parim(4));
