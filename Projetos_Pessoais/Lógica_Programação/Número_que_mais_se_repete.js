eeeba6eda4cd4fa5a5821ba6a7ec237826b934c8
DeleteMind <filipe.santos.cordeiro@gmail.com>
2025-05-05 13:09:14 +0100

    change_name_directory

---
+++ Projetos_Pessoais/Lógica_Programação/Número_que_mais_se_repete.js
@@ -0,0 +1,19 @@
+
+
+let array = [];
+
+
+function maisFrequente (input) {
+
+    array = input;
+    console.log(array);
+    for ( let i = 0 ; i <= array.length; i++) {
+        console.log(array[i]);
+    }
+}
+
+
+
+
+
+maisFrequente([1, 2, 3, 1, 5]);
