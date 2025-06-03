5a9430dab3835b4acee8c75ef740256556b6a6ae
DeleteMind <filipe.santos.cordeiro@gmail.com>
2025-06-03 14:36:22 +0100

    add_two_files

---
+++ Learning_C/prog0102.c
@@ -0,0 +1,4 @@
+#include <stdio.h> // Standard Input/Output
+main(){ // The program starts here
+    printf("Hello, Wolrd"); // Prints "Hello, World" using the printf() function (print formatted)
+}
