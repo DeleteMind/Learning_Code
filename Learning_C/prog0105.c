feef41609b421610f44dd081d1b9c23f1f40b739
DeleteMind <filipe.santos.cordeiro@gmail.com>
2025-06-04 11:33:50 +0100

    add_prog0105

---
+++ Learning_C/prog0105.c
@@ -0,0 +1,14 @@
+#include  <stdio.h>
+
+main() {
+    printf("Today is a \"good\" day!\n");
+    printf("This is a bell.\7\n");   /*Character "\7"  */
+    printf("And this is another bell.\a\n");  /*Character "\a" is the standard for bell */
+    printf("This is a backspace: back\bspace.\n"); /*Character "\b" */
+    printf("This is carriage return.\rStart\n"); /*Character "\r" */
+    printf("This is a horizonal tab.\tTest\n"); /*Character "\t" */
+    printf("This is a vertical tab\vTest\n"); /*Character "\v" */
+    printf("If I want to use \' and \" and \? and %%\n");
+}
+
+
